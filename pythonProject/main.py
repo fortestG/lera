df822a1559e58083ba38fdf1532322659eb5a778
adored <khaykin2001@mail.ru>
2022-11-17 20:14:42 +0300

    ploshad treugolnika

---
+++ pythonProject/main.py
@@ -0,0 +1,56 @@
+print ("vvedi x")
+x = int(input())
+print ("vvedi y")
+y = int(input())
+h = (x**2 - (y/2)**2)**0.5
+s = 0.5*y*h
+c = s
+print (s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
